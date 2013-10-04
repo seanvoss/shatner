7cd334dad4318300e5e12a8a21b43f0d384a3b14
Sean Voss <seanvoss@gmail.com>
2013-10-04 12:18:19 -0400

    Adding basic files

---
+++ name_price_wc.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: WooCommerce - Name your own price
+Plugin URI: http://seanvoss.com/wooprice
+Description: Allows users to set their own price (can also be used for donations) 
+Version: 0.1
+Author: Sean Voss
+Author URI: http://seanvoss.com/nameyourprice
+
+*/
+
+/*
+ * Title   : WooCommerce Name your own price
+ * Author  : Sean Voss
+ * Url     : http://seanvoss.com/wooprice
+ * License : http://seanvoss.com/wooprice/legal
+ */
+
+function name_price_wc_init() 
+{
+    /**
+     * Check if WooCommerce is active
+     **/
+    if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+        include_once('name_price_wc_plugin.php');
+    }
+}
+
+add_action('plugins_loaded', 'name_price_wc_init', 10);
---
+++ name_price_wc_plugin.php
@@ -0,0 +1,93 @@
+<?
+class WC_Donation extends WC_Cart
+{
+
+    public function __construct()
+    {
+        add_filter('woocommerce_get_price_html',           array($this,  'add_price_html'));
+        add_action('woocommerce_product_options_pricing',  array($this,  'add_donation_checkbox'));
+        add_action('save_post',                            array($this,  'set_named_price'));
+        add_action('woocommerce_add_to_cart',              array($this,  'add_to_cart_hook'));
+        add_action('woocommerce_before_calculate_totals',  array($this,  'add_custom_price' ));
+        add_action('init', array($this, 'init_css'));
+    }
+
+    public function init_css()
+    {
+        wp_register_style('donation_css', plugins_url('custom_styles.css',__FILE__ ), false, '1.0.0', 'all');
+        wp_enqueue_style( 'donation_css' );
+    }
+
+    public function add_to_cart_hook($key)
+    {
+        global $woocommerce;
+        foreach ($woocommerce->cart->get_cart() as $cart_item_key => $values) 
+        {
+            if($cart_item_key == $key)
+            {
+                $values['data']->set_price($_POST['price']);
+                $woocommerce->session->__set($key .'_named_price', $_POST['price']);
+            }
+        }
+
+    return $key;
+    }
+
+
+    public function set_named_price($post)
+    {
+        if($_POST['_own_price']){
+            if(!get_post_meta($_POST['post_ID'], '_own_price', true )){
+                add_post_meta($_POST['post_ID'], '_own_price', $_POST['_own_price']);
+            } else {
+                update_post_meta($_POST['post_ID'], '_own_price', $_POST['_own_price']);
+            }
+        } else {
+            if(get_post_meta($_POST['post_ID'], '_own_price', true )){
+                delete_post_meta($_POST['post_ID'], '_own_price');
+            }
+        }
+    }
+
+    public function add_donation_checkbox($content)
+    {
+       global $post ;
+       woocommerce_wp_checkbox(array('id' => '_own_price', 'class' => 'wc_own_price short', 'label' => __( 'Name you own price', 'woocommerce' ), 'value'=> 'yes', 'cbvalue' => get_post_meta($post->ID, '_own_price', true ) ? 'yes' : 'no'));
+    }
+
+    public function add_price_html($content)
+    {
+        global $post;
+        $post = get_post_meta($post->ID, '_own_price', true);
+        print "
+        <script>
+            jQuery(function($){
+                $('.cart').submit(function(){
+                    $('#price').clone().attr('type','hidden').appendTo($('form.cart'));
+                    return;
+                });
+            });
+        </script>";
+        if($post == 'yes')
+        {
+            return sprintf("<div class='name_price'><label>%s</label><input name='price' id='price' type='text' /></div>", "Name your own price");
+        }
+        return $content;
+        
+    }
+
+    public function add_custom_price( $cart_object ) {
+        global $woocommerce;
+        foreach ( $cart_object->cart_contents as $key => $value ) {
+
+                $named_price = $woocommerce->session->__get($key .'_named_price');
+                if($named_price)
+                {
+                    error_log(var_export($named_price,1));
+                    $value['data']->price = $named_price;
+                }
+        }
+    }
+
+}
+new WC_Donation();
